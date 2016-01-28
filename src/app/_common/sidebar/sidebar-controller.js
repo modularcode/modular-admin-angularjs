9a2be03dfa6cd20dc787f88ec1db2b9d387598aa
Aram Manukyan <aram.manukyan92@gmail.com>
2016-01-28 21:53:27 +0400

    Sidebar fixes

--- src/app/_common/sidebar/sidebar-controller.js
+++ src/app/_common/sidebar/sidebar-controller.js
@@ -4,15 +4,22 @@
 
 modularAdmin.app
 
-.controller("SidebarController", function($log, sidebarService) {
+.controller("SidebarController", function($log, $state, sidebarService) {
 
 	var vm = this;
 
-	vm.isVisible = false;
+	vm.isActive = function(states){
+		var currentState = $state.current.name;
+
+		if (states.indexOf(currentState) >= 0) {
+			return true;
+		}
+	}
 
 	/***************************************
 	*		   Sidebar Controls
 	****************************************/
+	vm.isVisible = false;
 	vm.controls = {};
 
 	vm.controls.show = function(){
